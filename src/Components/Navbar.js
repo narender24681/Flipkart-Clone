181efbb557d11f4a231550d873b2ac8a7113378b
agrawalrajat310 <agrawalrajat03@outlook.com>
2023-03-29 18:15:37 +0530

    added: done

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
     <div>
-        <Link to="/">Home</Link>
+      <Link to="/">Home</Link>
     </div>
-  )
-}
+  );
+};
