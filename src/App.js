7ea32baa3fc2e731a1e1ffee8138bd412a68a766
Narender <narender24681@gmail.com>
2023-04-01 16:03:44 +0530

    Changed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import "./App.css";
-
 import { AllRoutes } from "./Components/AllRoutes";
-import { Navbar } from "./Components/Navbar";
 
 function App() {
   return (
     <div>
-
-      {/* <Navbar /> */}
-
       <AllRoutes />
 
     </div>
