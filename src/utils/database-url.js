16c07601418d45f56f9b310824334e4ec19a9d94
sumit <sumitkolipyaka99@gmail.com>
2023-05-15 20:00:30 +0530

    Done with final codeaa

--- src/utils/database-url.js
+++ src/utils/database-url.js
@@ -1,2 +1,2 @@
-export const databaseUrl = `http://localhost:8088`;
+export const databaseUrl = `https://smily-kart.onrender.com`;
 // export const databaseUrl = `https://json-server-flipkart.onrender.com`;
